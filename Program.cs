46f59244cf86cb0698da5f28f38f331907eb8cef
Viktor Tiahur <abik2002@ukr.net>
2024-12-13 15:35:21 +0100

    13.12.2024

--- Program.cs
+++ Program.cs
@@ -4,38 +4,49 @@ using System.Dynamic;
 using System.Reflection.Metadata;
 using System.Xml.Schema;
 
-public class ArreyExample
+public class searchStudent
 {
     public static void Main()
     {
-        var filmografphy = new int[10];
-        filmografphy[0] = 10;
-        filmografphy[1] = 7;
-        filmografphy[2] = 23;
 
-        for (int i = 3; i < 10; i++)
+        string[] studantName = { "Smith", "Olaf", "Jon", "Auron", "Melisa", "Cirkul", "Shmon", "Arnold" };
+
+        Console.WriteLine("Write student name");
+        string newName = Console.ReadLine();
+
+        bool isNAmeFound = CheckNAmeArrey(studantName, newName);
+        Console.WriteLine(isNAmeFound ? $"Студент {newName} присутній" : "False");
+
+
+        static bool CheckNAmeArrey(string[] studantName, string newName)
         {
-            Console.WriteLine($"The number of movies with superhiro #{i + 1} is: ");
-            filmografphy[i] = Convert.ToInt32(Console.ReadLine());
-        }
-
-        int totalMovies = 0;
-        foreach (int num in filmografphy)
-        {
-            totalMovies += num;
-        }
-
-        Console.WriteLine($"The total number of movies with all superheroes: {totalMovies}");
-
-        int min = filmografphy[0];
-        for (int i = 0; i < filmografphy.Length; i++)
-        {
-            if (filmografphy[i] < min)
+            foreach (string name in studantName)
             {
-                min = filmografphy[i];
+                if (name == newName)
+                {
+                    return true;
+                }
+
+
+
             }
+            return false;
         }
-        Console.WriteLine($"The smallest filmography consist of {min} movies");
+
+        // foreach (string Name in studantName)
+        // {
+        //     if (Name == newName)
+        //     {
+        //         Console.WriteLine($"Student {newName} was in clasroom");
+        //         break;
+        //     }
+        //     if ()
+        //     {
+
+        //         Console.WriteLine($"The student {newName} is absent from class");
+        //     }
+        // }
+
 
     }
 
--- obj/Debug/net8.0/ProjectC#.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6340e345ad1ff5aa5e50744bf363b3f851f9f3d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf208e3b01ed161b23a85de3da723ebc229fe72a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
