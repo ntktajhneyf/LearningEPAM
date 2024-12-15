96a5fe1b6ff535118cdd835acde06b0d9fc5e83b
Viktor Tiahur <abik2002@ukr.net>
2024-12-15 19:28:33 +0100

    learning HTML, CSS 15,12

--- obj/Debug/net8.0/ProjectC#.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf208e3b01ed161b23a85de3da723ebc229fe72a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64454def9799f9573e2fd16422bf55641714a330")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
